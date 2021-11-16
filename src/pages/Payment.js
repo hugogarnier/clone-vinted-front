e5c0f3ff9524475ca77459821792277eae050879
Hugo Garnier <h.garnier@outlook.com>
2021-11-16 16:08:46 +0100

    env

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -3,10 +3,7 @@ import { Elements } from "@stripe/react-stripe-js";
 
 import Checkout from "../components/Checkout";
 
-// const stripePromise = loadStripe(`${process.env.REACT_APP_STRIPE_PUBLIC}`);
-const stripePromise = loadStripe(
-  `pk_test_51JwPw8FirJK8CvY70vMIM3xBAXXdAQwTaXS7ISsTswb0lM8a9musCa4r4BBqpAOLUxuo3hODwA2LLGXkriviYPW700NqGsz3h5`
-);
+const stripePromise = loadStripe(`${process.env.REACT_APP_STRIPE_PUBLIC}`);
 
 const Payment = ({ token }) => {
   return (
